e445046e6836354004988e697679a9bc78162694
Griseliz <git-commit-address>
2022-12-12 11:28:42 -0400

    import app views

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" View index: status message"""
+from flask import Flask, Blueprint, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def _status():
+    """returns a JSON file with Status: OK"""
+    return jsonify({"status": "OK"})
