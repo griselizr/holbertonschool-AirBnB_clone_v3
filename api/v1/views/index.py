6449fe6726bea9f7df54350bb25760357c3f6cad
Griseliz <git-commit-address>
2022-12-12 12:20:01 -0400

    add a method to return count of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ View index: status message"""
-
+import models
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 
@@ -10,3 +10,21 @@ def _status():
     """returns a JSON file with Status: OK"""
 
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """ Returns count of objects by type """
+    utilities_dic = {}
+
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"}
+
+    for k, val in classes.items():
+        utilities_dic[val] = models.storage.count(k)
+        return jsonify(utilities_dic)
