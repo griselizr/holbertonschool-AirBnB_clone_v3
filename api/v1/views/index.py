e3cf5a68a368ef11c6270ab2acff64b8bce3a0d3
Griseliz <git-commit-address>
2022-12-12 11:49:34 -0400

    fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ View index: status message"""
+
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 
@@ -7,4 +8,5 @@ from api.v1.views import app_views
 @app_views.route('/status', strict_slashes=False)
 def _status():
     """returns a JSON file with Status: OK"""
+
     return jsonify({"status": "OK"})
